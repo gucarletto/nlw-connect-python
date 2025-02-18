d631b7c7e8cf058d32b60701912657b2c1253eef
Gustavo Carletto <gucarletto@gmail.com>
2025-02-18 11:55:16 -0300

    database modeling

---
+++ src/model/entities/inscritos.py
@@ -0,0 +1,10 @@
+from src.model.config.base import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+class Inscritos(Base):
+  __tablename__ = 'Inscritos'
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  nome = Column(String, nullable=False)
+  email = Column(String, nullable=False)
+  link = Column(String)
+  evento_id = Column(Integer, ForeignKey('Eventos.id'), nullable=False)
