5f70b9e77b823dc3177a7828f8995b3dcf1e8c3b
Gustavo Carletto <gucarletto@gmail.com>
2025-02-19 11:44:11 -0300

    feat: register routes and fixes

--- src/main/routes/subscriptions.py
+++ src/main/routes/subscriptions.py
@@ -1,18 +1,18 @@
 from flask import Blueprint, jsonify, request
 
-event_route_bp = Blueprint('suscriptions_route', __name__)
+subscription_route_bp = Blueprint('suscriptions_route', __name__)
 
 from src.validators.subscribers_creator_validator import subscribers_creator_validator
 from src.http_types.http_request import HttpRequest
 from src.controllers.subscribers.susbcribers_creator import SubscribersCreator
 from src.model.repositories.subscribers_repository import SubscribersRepository
 
-@event_route_bp.route('/subscription', methods=['POST'])
+@subscription_route_bp.route('/subscription', methods=['POST'])
 def create_new_subscription():
   subscribers_creator_validator(request.json)
   http_request = HttpRequest(body=request.json)
 
-  events_creator = SubscribersCreator(SubscribersRepository())
-  http_response = events_creator.create_subscription(http_request)
+  subs_creator = SubscribersCreator(SubscribersRepository())
+  http_response = subs_creator.create_subscription(http_request)
 
   return jsonify(http_response.body), http_response.status_code
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,6 +1,8 @@
 from src.main.routes.event import event_route_bp
+from src.main.routes.subscriptions import subscription_route_bp
 from flask import Flask
 
 app = Flask(__name__)
 
 app.register_blueprint(event_route_bp)
+app.register_blueprint(subscription_route_bp)
