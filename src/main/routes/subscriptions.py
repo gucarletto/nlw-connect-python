243c8510fc2828372f829b53b7d731fd07db5590
Gustavo Carletto <gucarletto@gmail.com>
2025-02-20 11:13:15 -0300

    adding last features on event linking

--- src/main/routes/subscriptions.py
+++ src/main/routes/subscriptions.py
@@ -6,6 +6,7 @@ from src.validators.subscribers_creator_validator import subscribers_creator_val
 from src.http_types.http_request import HttpRequest
 from src.controllers.subscribers.susbcribers_creator import SubscribersCreator
 from src.model.repositories.subscribers_repository import SubscribersRepository
+from src.controllers.subscribers.susbcribers_manager import SubscriberManager
 
 @subscription_route_bp.route('/subscription', methods=['POST'])
 def create_new_subscription():
@@ -14,5 +15,27 @@ def create_new_subscription():
 
   subs_creator = SubscribersCreator(SubscribersRepository())
   http_response = subs_creator.create_subscription(http_request)
+
+  return jsonify(http_response.body), http_response.status_code
+
+@subscription_route_bp.route('/subscription/link/<link>/event/<event_id>', methods=['GET'])
+def susbscribers_by_link(link, event_id):
+  subs_repo = SubscribersRepository()
+  subs_manager = SubscriberManager(subs_repo)
+
+  http_request = HttpRequest(params={'link': link, "event_id": event_id})
+
+  http_response = subs_manager.get_subscribers_by_link(http_request)
+
+  return jsonify(http_response.body), http_response.status_code
+
+@subscription_route_bp.route('/subscription/ranking/event/<event_id>', methods=['GET'])
+def susbscribers_link_ranking(event_id):
+  subs_repo = SubscribersRepository()
+  subs_manager = SubscriberManager(subs_repo)
+
+  http_request = HttpRequest(params={"event_id": event_id})
+
+  http_response = subs_manager.get_ranking(http_request)
 
   return jsonify(http_response.body), http_response.status_code
--- src/model/repositories/subscribers_repository.py
+++ src/model/repositories/subscribers_repository.py
@@ -1,6 +1,7 @@
 from src.model.configs.connection import DBConnectionHandler
 from src.model.entities.inscritos import Inscritos
 from .interfaces.subscribers_repository import SubscribersRepositoryInterface
+from sqlalchemy import func, desc
 
 class SubscribersRepository(SubscribersRepositoryInterface):
   def insert(self, subscriber_infos: dict) -> None:
@@ -21,3 +22,22 @@ class SubscribersRepository(SubscribersRepositoryInterface):
   def select_subscriber(self, subscriber_email: str, event_id: int) -> Inscritos:
     with DBConnectionHandler() as db:
       return db.session.query(Inscritos).filter_by(email=subscriber_email, evento_id=event_id).first()
+
+  def select_subscribers_by_link(self, link: str, event_id: int) -> list:
+    with DBConnectionHandler() as db:
+      return db.session.query(Inscritos).filter_by(link=link, evento_id=event_id).all()
+
+  def get_ranking(self, event_id: int) -> tuple:
+    with DBConnectionHandler() as db:
+      result = (
+        db.session.query(
+          Inscritos.link,
+          func.count(Inscritos.link)).label('total')
+        .filter(
+          Inscritos.evento_id == event_id,
+          Inscritos.link != None
+        )
+        .group_by(Inscritos.link)
+        .order_by(desc('total'))
+        .all()
+      )
